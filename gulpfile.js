a82205f420992c69f034a8f7bd62d47a45983db2
John Preston <wcerfgba@riseup.net>
2016-06-26 21:14:28 +0100

    WebExtension packaging.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 var gulp = require('gulp');
 var hb = require('gulp-hb');
 var sass = require('gulp-sass');
+var run = require('gulp-run');
+var concat = require('gulp-concat');
 
 gulp.task('default', [ 'webextension', 'website' ]);
 
-gulp.task('webextension', [ 'webextension_unpacked' ]);
+gulp.task('webextension', [ 'webextension_unpacked', 'webextension_firefox',
+                            'webextension_chrome', 'webextension_bookmarklet' ]);
 
 gulp.task('webextension_unpacked', function () {
-  gulp.src('webextension/src/**')
+  return gulp.src('webextension/src/**')
+          .pipe(gulp.dest('webextension/build/unpacked/'));
+});
+
+gulp.task('webextension_firefox', [ 'webextension_unpacked' ], function () {
+  run('cd webextension/build/unpacked/ ; rm ../slink_firefox.xpi ; zip -r ../slink_firefox.xpi *')
+    .exec();
+});
+
+gulp.task('webextension_chrome', [ 'webextension_unpacked' ], function () {
+  run('cd webextension/build/ ; chromium --pack-extension=unpacked ; mv unpacked.crx slink_chrome.crx ; mv unpacked.pem slink_chrome_private_key.pem')
+    .exec();
+});
+
+gulp.task('webextension_bookmarklet', [ 'webextension_unpacked' ], function () {
+  gulp.src('webextension/build/unpacked/slink.js')
+    .pipe(concat('slink_bookmarklet.js'))
     .pipe(gulp.dest('webextension/build/'));
 });
 
